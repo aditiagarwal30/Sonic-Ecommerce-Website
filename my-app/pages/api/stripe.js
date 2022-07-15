767a237d22f923086eec5b039402fdd5249761d9
Aditi Agarwal <aditi2k1@gmail.com>
2022-07-16 03:40:03 +0530

    Update stripe.js

--- my-app/pages/api/stripe.js
+++ my-app/pages/api/stripe.js
@@ -12,8 +12,8 @@ export default async function handler(req, res) {
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_options: [
-          { shipping_rate: 'shr_1LLufOSEQ3CtBDfewVXVQr0Q' },
-          { shipping_rate: 'shr_1LLuh4SEQ3CtBDfeidRy7cWh' },
+          { shipping_rate: 'shr_1LLweqSC1AS31vnxeAP7MdKJ' },
+          { shipping_rate: 'shr_1LLwfdSC1AS31vnxW4ytASiI' },
         ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
